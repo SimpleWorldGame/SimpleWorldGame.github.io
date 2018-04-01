89cec81c53fdf6b1b527bf1ce8b30e0c17750d76
SilentHarmonyYT <37558286+SilentHarmonyYT@users.noreply.github.com>
2018-04-01 09:01:26 -0500

    Update actor.js

--- scripts/actor.js
+++ scripts/actor.js
@@ -2,11 +2,24 @@ var moneyhour = 26;
 var money;
 var moneymade = document.getElementById("money");
 function job(){
-  setTimeout
+  setTimeout(addone, 120000);
+  setTimeout(addtwo, 240000);
+  setTimeout(addthree, 360000);
+  setTimeout(addfour, 480000);
+  setTimeout(addfive, 600000);
 }
 function addone(){
   moneymade.innerHTML = "$" + moneyhour;
 }
 function addtwo(){
   moneymade.innerHTML = "$" + moneyhour + moneyhour;
+}
+function addthree(){
+  moneymade.innerHTML = "$" + moneyhour + moneyhour + moneyhour;
+}
+function addfour(){
+  moneymade.innerHTML = "$" + moneyhour + moneyhour + moneyhour + moneyhour;
+}
+function addfive(){
+  moneymade.innerHTML = "$" + moneyhour + moneyhour + moneyhour + moneyhour + moneyhour;
 }
