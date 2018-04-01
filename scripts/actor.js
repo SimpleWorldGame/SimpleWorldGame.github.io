fb1a8c66d15c2efd46f91496188e50db6eef1a47
SilentHarmonyYT <37558286+SilentHarmonyYT@users.noreply.github.com>
2018-04-01 08:57:05 -0500

    Update actor.js

--- scripts/actor.js
+++ scripts/actor.js
@@ -1,1 +1,12 @@
-
+var moneyhour = 26;
+var money;
+var moneymade = document.getElementById("money");
+function job(){
+  setTimeout
+}
+function addone(){
+  moneymade.innerHTML = "$" + moneyhour;
+}
+function addtwo(){
+  moneymade.innerHTML = "$" + moneyhour + moneyhour;
+}
